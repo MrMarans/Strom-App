e069f600291a07baa9b7df421217af9976c9cb92
MrMaransm <ron_marvin@yahoo.de>
2024-10-06 16:28:58 +0200

    changed functionality to use matplotlib

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ import numpy as np
 import json
 import os
 from datetime import datetime
+import matplotlib.pyplot as plt
 
 current_year = datetime.now().year
 current_directory = os.getcwd()
-
-chart = st.line_chart()
+# plot = st.pyplot()
+#chart = st.line_chart()
 
 Month_translator = {
     "Januar": "01",
@@ -25,6 +26,8 @@ Month_translator = {
     "November": "11",
     "Dezember": "12"
 }
+
+
 
 def loadChart(chosenYear):
     years_data = {}
@@ -42,24 +45,59 @@ def loadChart(chosenYear):
                     st.write("Error loading")
 
 
+    # Daten generieren
+    strom = []
+    pv_mini = []
+    pv = []
+    y = []
+    fig, ax = plt.subplots(figsize=(10, 6))
     if chosenYear == "Alle":
-        chart.line_chart(data=years_data, x_label="Monat", y_label="Strommenge")
-    
-    elif chosenYear == "Zeitlinie":
-        timeline = {} 
-        for year, data in years_data.items():
-            for month, value in data.items():
-                timeline[f"{year}_{month}"] = value
-            
-        chart.line_chart(data=timeline, x_label="Monat", y_label="Strommenge")
-    else:
-        chart.line_chart(data=years_data[chosenYear], x_label="Monat", y_label="Strommenge")
+        years = []
+        for year, monthData in years_data.items():
+            for month, data in monthData.items():
+                pass
 
+
+
+    elif chosenYear == "Zeitlinie":
+        for year, monthData in years_data.items():
+            for month, data in monthData.items():
+                # print(f"{year} was year,  {month} was month, {data} was data")
+                strom.append(data["strom"])
+                pv_mini.append(data["PV_Mini"])
+                pv.append(data["PV"])
+                y.append(f"{year-2000}_{month}")
+
+    else:
+        for month, data in years_data[chosenYear].items():
+            strom.append(data["strom"])
+            pv_mini.append(data["PV_Mini"])
+            pv.append(data["PV"])
+            y.append(month)
+
+    ax.plot(y, strom, label='Monat')
+    ax.plot(y ,pv_mini, label = "PV mini")
+    ax.plot(y ,pv, label= "PV")
+
+    for i in enumerate(y):
+        ax.text(strom,y,i, ha="center", va="center")
+
+    # Diagramm beschriften
+    ax.set_title('Stromdaten')
+    ax.set_xlabel('Monat')
+    ax.set_ylabel('Strommenge')
+    ax.legend()
+    ax.grid(True)
+
+    # Diagramm in Streamlit anzeigen
+    plot = st.pyplot(fig)
+    
+    return plot #?
 
 
 
 def drawYearsRadio():
-    years = ["Alle", "Zeitlinie"]
+    years = ["Zeitlinie"]
     for year in range (35):
             year += 2000 
             filename = f"y{year}.json"
@@ -72,10 +110,10 @@ def drawYearsRadio():
         years,
         )
     
-    if chosenYear == "Alle":
-        passYear = "Alle"
-    else: 
-        passYear = chosenYear
+    # if chosenYear == "Alle":
+        # passYear = "Alle"
+    # else: 
+    passYear = chosenYear
     loadChart(passYear)
 
     
@@ -88,6 +126,8 @@ def drawInputs():
     month = st.selectbox("Wähle einen Monat:", ["Bitte Auswählen", "Januar","Februar","März","April","Mai","Juni","Juli","August","Sepember","Oktober","November","Dezember"])
 
     strom = st.number_input("Menge:", value=0)
+    pv_Mini= st.number_input("PV Mini:", value=0)
+    pv= st.number_input("PV:", value=0)
 
 
     if st.button("Daten speichern"):
@@ -97,16 +137,17 @@ def drawInputs():
                 if filename in os.listdir(current_directory):
                     with open(filename) as loadedFile:
                         old = json.load(loadedFile)
-                        old[str(Month_translator[month])] = strom
+                        old[month] = {"strom": strom, "PV_Mini": pv_Mini, "PV": pv}
 
+                #? new file creating
                 else:
                     st.write(f"new file for year 20{year} created")
-                    old = {str(Month_translator[month]):strom}
+                    old = {month : {"strom": strom, "PV_Mini": pv_Mini, "PV": pv}}
                 
                 with open(filename,'w') as file:
                     json.dump(old, file)
                 
-                loadChart("Alle")
+                # loadChart("Zeitlinie")
 
 
             else: st.write(f"Bitte noch den Monat auswählen")
@@ -116,5 +157,5 @@ def drawInputs():
 
 
 
-loadChart("Alle")
+# plot = loadChart("Zeitlinie")
 drawInputs()
